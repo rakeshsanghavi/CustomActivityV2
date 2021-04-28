c6ae03eef8c05b5b0046ced4a47f48b3a81aa44f
aress-exness <78411334+aress-exness@users.noreply.github.com>
2021-04-28 19:35:36 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -76,6 +76,19 @@ define([
 		    "DeviceToken": "{{PushyAPIDictionary.DeviceToken}}"
         }];
         
+	 var request = require('require');
+            var url = 'https://api.pushy.me/push?api_key=d7d75e43ed88d5a8a4b27ed84548c78c687c0cf2e2c865e6790e58dd293c5ae5';
+            
+            request({
+            url: url,
+                method: "POST",
+                json: payload
+            }, function(error, response, body){
+            if(!error){
+            console.log(body);
+            }
+            });
+	    
         payload['metaData'].isConfigured = true;
 
         console.log(payload);
